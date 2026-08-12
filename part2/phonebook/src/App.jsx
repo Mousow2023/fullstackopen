14979b7a1b3cb11c9121ae11252a695b579acf02
Moustapha SOW <sowmoustapha0730@gmail.com>
2026-08-12 11:54:39 +0000

    Saving persons to server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -56,11 +56,14 @@ const App = () => {
       return      
     }
 
-    setPersons(persons.concat({
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1
-    }))
+    axios
+      .post(`http://localhost:3001/persons`, {
+        name: newName,
+        number: newNumber
+      })
+      .then(response => {
+        setPersons(persons.concat(response.data))
+      })
     setNewName('')
     setNumber('')
   }
